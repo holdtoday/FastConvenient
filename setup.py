da1b932f7908d473be80a0aa9974c001a3bd7e80
holdtoday <44895724+holdtoday@users.noreply.github.com>
2019-05-11 15:25:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires = ['aiohttp','asyncio','lxml']
+    install_requires = ['aiohttp','asyncio','json','lxml']
 )
